e2e95056d09c1d29a9a97766dc6039520eae3b97
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-18 16:08:40 -0800

    40.1 completed 11/18/24

---
+++ src/dkeeper_frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import HighlightIcon from '@material-ui/icons/Highlight';
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
